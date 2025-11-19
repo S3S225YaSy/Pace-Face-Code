11058e10cedd2c8ca5e1ffd6334a97a5e38f6d43
KCSNET\st20232205 <20232205@kcska.onmicrosoft.com>
2025-11-19 16:54:04 +0900

    ユーザー設定画面の追加

---
+++ app/src/main/java/com/example/paceface/UserSettingsScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.example.paceface
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class UserSettingsScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.user_settings_screen)
+    }
+}
