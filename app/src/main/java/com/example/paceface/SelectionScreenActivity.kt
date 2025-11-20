df13f9046d581498ddb0247d60571ff2e8a6e0a2
Koyasikitakummi <20233132@kcska.onmicrosoft.com>
2025-11-20 12:22:56 +0900

    選択画面画面付き(仮)完成

--- app/src/main/java/com/example/paceface/SelectionScreenActivity.kt
+++ app/src/main/java/com/example/paceface/SelectionScreenActivity.kt
@@ -1,4 +1,11 @@
-package com.example.paceface;
+package com.example.paceface
 
-public class SelectionScreenActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SelectionScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.selection_screen)
+    }
 }
